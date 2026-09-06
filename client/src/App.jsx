1faa1a7b5edc4337f59ffc6fed1d8fc2e4f87876
liamalvin505-sketch <liamalvin505@gmail.com>
2026-09-06 08:07:31 +0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,10 @@
-
 import React from 'react';
-import AllInOneCasino from './components/AllInOneCasino';
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <AllInOneCasino />
+    <div style={{ textAlign: 'center', marginTop: '50px', color: '#fff', background: '#0b0f19', height: '100vh', padding: '20px' }}>
+      <h1>Gambles Casino Lobby</h1>
+      <p>System is running successfully!</p>
     </div>
   );
 }
-
-export default App;
