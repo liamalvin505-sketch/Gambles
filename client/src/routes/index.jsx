f29d6daf5d0c18597d9aceb4c7bb0f173cc23514
liamalvin505-sketch <liamalvin505@gmail.com>
2026-09-06 07:39:18 +0600

    Update index.jsx

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,74 +1,16 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "../App.jsx";
-import Home from "../pages/Home.jsx";
-import VerifyEmail from "../pages/VerifyEmail.jsx";
-import RegisterPage from "../pages/RegisterPage.jsx";
-import LoginPage from "../pages/LoginPage.jsx";
-import ProtectedRedirect from "../components/ProtectedRedirect.jsx";
-import ForgotPassword from "../pages/ForgotPassword.jsx";
-import VerifyResetPasswordOTP from "../pages/VerifyResetPasswordOTP.jsx";
-import SetNewPassword from "../pages/SetNewPassword.jsx";
-import OauthSuccess from "../components/OauthSuccess.jsx";
-import Profile from "@/pages/Profile.jsx";
-import DepositMoney from "@/pages/DepositMoney.jsx";
-import WithdrawMoney from "@/pages/WithdrawMoney.jsx";
-import Games from "@/pages/Games.jsx";
-import MinesGame from "@/pages/Games/MinesGame.jsx";
-import Dashboard from "@/pages/Dashboard.jsx";
-import DiceGame from "@/pages/Games/DiceGame.jsx";
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import AllInOneCasino from '../components/AllInOneCasino';
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        children: [
-            { path: "", element: <Home /> },
-            // {
-            //     path: "register",
-            //     element: (
-            //         <ProtectedRedirect>
-            //             <RegisterPage />
-            //         </ProtectedRedirect>
-            //     ),
-            // },
-            // {
-            //     path: "login",
-            //     element: (
-            //         <ProtectedRedirect>
-            //             <LoginPage />
-            //         </ProtectedRedirect>
-            //     ),
-            // },
-            { path: "forgot-password", element: <ForgotPassword /> },
-            { path: "verify-forgot-password-otp", element: <VerifyResetPasswordOTP /> },
-            { path: "set-new-password", element: <SetNewPassword /> },
-            { path: "oauth-success", element: <OauthSuccess /> },
-            { path: "profile", element: <Profile /> },
-            { path: "dashboard", element: <Dashboard /> },
-            { path: "deposit-money", element: <DepositMoney /> },
-            { path: "withdraw-money", element: <WithdrawMoney /> },
-            { path: "games", element: <Games /> },
-            { path: "games/mines", element: <MinesGame /> },
-            { path: "games/dice", element: <DiceGame /> },
-        ],
-    },
-    { path: "verify-user", element: <VerifyEmail /> },
-    {
-        path: "register",
-        element: (
-            <ProtectedRedirect>
-                <RegisterPage />
-            </ProtectedRedirect>
-        ),
-    },
-    {
-        path: "login",
-        element: (
-            <ProtectedRedirect>
-                <LoginPage />
-            </ProtectedRedirect>
-        ),
-    },
+  {
+    path: "/",
+    element: <AllInOneCasino />,
+  },
+  {
+    path: "*",
+    element: <AllInOneCasino />,
+  }
 ]);
 
 export default router;
