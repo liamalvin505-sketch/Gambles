8bb9320523fe3b368725225a9db68ec5f868c4e9
liamalvin505-sketch <liamalvin505@gmail.com>
2026-09-06 07:14:26 +0600

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { RouterProvider } from 'react-router-dom';
-import router from './routes/index.jsx';
-import { Toaster } from 'react-hot-toast';
-import { UserProvider } from './contexts/UserContext';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <UserProvider>
-    <RouterProvider router={router} />
-    <Toaster />
-  </UserProvider>
+  <App />
 );
